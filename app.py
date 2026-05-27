b0685d32f279710aabc301bf74f786d84496c5ec
kop1ka <annabatuhno15@gmail.com>
2026-05-28 00:47:55 +0500

    Update app.py

--- app.py
+++ app.py
@@ -513,8 +513,8 @@ def get_safe_redirect_url(next_page):
         return URL_PREFIX + '/admin'
     
     # Добавляем префикс к next_page если он отсутствует
-   if not next_page.startswith(URL_PREFIX):
-       return URL_PREFIX + '/' + next_page.lstrip('/')
+    if not next_page.startswith(URL_PREFIX):
+        return URL_PREFIX + '/' + next_page.lstrip('/')
     
     return next_page
 
